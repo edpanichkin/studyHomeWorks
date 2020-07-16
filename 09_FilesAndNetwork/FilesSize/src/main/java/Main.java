b6d57b6f1d60af248e392403c2c6a89aa9af8f8b
EDPanichkin <edpanichkin@gmail.com>
2020-07-16 13:23:06 +0300

    task done

--- 09_FilesAndNetwork/FilesSize/src/main/java/Main.java
+++ 09_FilesAndNetwork/FilesSize/src/main/java/Main.java
@@ -1,27 +1,48 @@
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 
 public class Main {
-    public static void main(String[] args) {
-        File folder;
-        try {
-            System.out.println("Введите путь к папке:");
 
-            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-            //String inputPath = reader.readLine().replace("\\", File.separator);
-            folder = new File("D:\\Videos_LG");
-            File[] files = folder.listFiles();
-            for (File file: files){
-                System.out.println(file.getAbsolutePath() + " " + file.length() + " "+ file.isDirectory());
+    private static File directory;
+    public static void main(String[] args) throws IOException{
+        System.out.println("Введите путь к папке:");
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        directory = new File(reader.readLine());
+        sizeCounter(directory);
+    }
+    public static long sizeInDirCounter (File folder){
+        File[] files = folder.listFiles();
+        long bytes = 0;
 
+            for (File file : files) {
+                try {
+                    if (!file.isDirectory()) {
+                        bytes += file.length();
+                    } else {
+                        bytes += sizeInDirCounter(new File(file.getAbsolutePath()));
+                    }
+                } catch (Exception ex) {
+                    System.out.println(ex.getMessage() + " " + file.getAbsolutePath());
+                }
             }
+        return bytes;
+    }
 
+    public static void sizeConverter (long sizeInBytes) {
+        String[] lettersForSize = {"B", "kB", "MB", "GB", "TB"};
+        int i = (int) Math.floor(Math.log10(sizeInBytes) / 3);
+        System.out.printf("Директория %s размером: %.2f %s", directory,
+                sizeInBytes / Math.pow(1024, i), lettersForSize[i]);
+    }
 
+    public static void sizeCounter (File folder){
+        if (folder != null && folder.exists() && folder.isDirectory()) {
+            sizeConverter(sizeInDirCounter(folder));
         }
-        catch (Exception ex) {
-            ex.printStackTrace();
+        else {
+            System.out.println("Ошибка ввода!");
         }
     }
 }
