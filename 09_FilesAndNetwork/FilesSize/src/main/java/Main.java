c8915572806ce02b9529c8d0c31bb2d573c7079b
EDPanichkin <edpanichkin@gmail.com>
2020-07-18 08:25:12 +0300

    args + NIO

--- 09_FilesAndNetwork/FilesSize/src/main/java/Main.java
+++ 09_FilesAndNetwork/FilesSize/src/main/java/Main.java
@@ -1,39 +1,57 @@
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.nio.file.*;
 
 public class Main {
 
     private static File directory;
     public static void main(String[] args) throws IOException{
-        System.out.println("Введите путь к папке:");
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        directory = new File(reader.readLine());
-        sizeCounter(directory);
+        //System.out.println("Введите путь к папке:");
+        //BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        //directory = new File(reader.readLine());
+        if (args.length > 0) {
+            directory = new File(args[0]);
+            System.out.println("IO");
+            sizeCounter(directory);
+            try {
+                long sizeDirNIO = Files.walk(Paths.get(directory.getAbsolutePath()))
+                        .map(Path::toFile)
+                        .filter(File::isFile)
+                        .mapToLong(File::length)
+                        .sum();
+                System.out.println("NIO");
+                sizeConverter(sizeDirNIO);
+            } catch (Exception ex) {
+                System.out.println(ex.getMessage());
+            }
+        }
+        else{
+            System.out.println("NO INPUT ARGS!");
+        }
+
     }
+
     public static long sizeInDirCounter (File folder){
         File[] files = folder.listFiles();
         long bytes = 0;
 
-            for (File file : files) {
-                try {
-                    if (!file.isDirectory()) {
-                        bytes += file.length();
-                    } else {
-                        bytes += sizeInDirCounter(new File(file.getAbsolutePath()));
-                    }
-                } catch (Exception ex) {
-                    System.out.println(ex.getMessage() + " " + file.getAbsolutePath());
+        for (File file : files) {
+            try {
+                if (!file.isDirectory()) {
+                    bytes += file.length();
+                } else {
+                    bytes += sizeInDirCounter(new File(file.getAbsolutePath()));
                 }
+            } catch (Exception ex) {
+                System.out.println(ex.getMessage() + " " + file.getAbsolutePath());
             }
+        }
         return bytes;
     }
 
     public static void sizeConverter (long sizeInBytes) {
         String[] lettersForSize = {"B", "kB", "MB", "GB", "TB"};
         int i = (int) Math.floor(Math.log10(sizeInBytes) / 3);
-        System.out.printf("Директория %s размером: %.2f %s", directory,
+        System.out.printf("Directory %s size: %.2f %s \n", directory,
                 sizeInBytes / Math.pow(1024, i), lettersForSize[i]);
     }
 
@@ -42,7 +60,7 @@ public class Main {
             sizeConverter(sizeInDirCounter(folder));
         }
         else {
-            System.out.println("Ошибка ввода!");
+            System.out.println("INPUT ERROR! " + directory.toString());
         }
     }
 }
