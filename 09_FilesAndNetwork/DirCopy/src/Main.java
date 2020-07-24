be1c82b26ee5a3728a4be25c0a127f68a16fb474
EDPanichkin <edpanichkin@gmail.com>
2020-07-24 23:04:07 +0300

    Files.walkFileTree

--- 09_FilesAndNetwork/DirCopy/src/Main.java
+++ 09_FilesAndNetwork/DirCopy/src/Main.java
@@ -2,13 +2,33 @@ import java.io.*;
 import java.nio.file.*;
 import java.nio.file.attribute.BasicFileAttributes;
 
-public class Main extends SimpleFileVisitor {
+public class Main{
 
     static Path pathSource = Paths.get("D:\\test");
     static Path pathTarget = Paths.get("D:\\test2");
 
     public static void main(String[] args) throws IOException {
-        filesAndDirsFullCopy(pathSource.toFile());
+//     filesAndDirsFullCopy(pathSource.toFile());  // Version 1 - commited & work
+        try{
+            Files.walkFileTree(pathSource, new SimpleFileVisitor<>() {
+                @Override
+                public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                    dirCreate(pathChange(file.toString()));
+                    Files.copy(file.toAbsolutePath(), pathChange(file.toString()), StandardCopyOption.REPLACE_EXISTING);
+                    return FileVisitResult.CONTINUE;
+                }
+            });
+        }
+        catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public static void dirCreate(Path file) throws IOException {
+        Path strPath = Paths.get(file.toString().replace(file.getFileName().toString(),""));
+        if (!Files.exists(strPath)) {
+            Files.createDirectory(strPath);
+        }
     }
 
     public static void filesAndDirsFullCopy (File folder) throws IOException {
@@ -34,6 +54,7 @@ public class Main extends SimpleFileVisitor {
             }
         }
     }
+
     public static Path pathChange (String file){
         return Paths.get(file.replace(pathSource.toString(),pathTarget.toString()));
     }
