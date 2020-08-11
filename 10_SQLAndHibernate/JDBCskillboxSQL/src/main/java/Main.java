c53e6580c2fc5202c7312653bfcfa57ad5f346c7
EDPanichkin <edpanichkin@gmail.com>
2020-08-11 11:16:46 +0300

    add try-with-resources

--- 10_SQLAndHibernate/JDBCskillboxSQL/src/main/java/Main.java
+++ 10_SQLAndHibernate/JDBCskillboxSQL/src/main/java/Main.java
@@ -1,30 +1,24 @@
 import java.sql.*;
 
 public class Main {
-    public static void main(String[] args){
-        try {
-            String url = "jdbc:mysql://localhost:3306/skillbox?useSSL=false&serverTimezone=UTC";
-            String user = "root";
-            String password = "testtest";
-            Connection connection = DriverManager.getConnection(url, user, password);
+    public static void main(String[] args) throws SQLException {
+        String url = "jdbc:mysql://localhost:3306/skillbox?useSSL=false&serverTimezone=UTC";
+        String user = "root";
+        String password = "testtest";
+
+        try(Connection connection = DriverManager.getConnection(url, user, password);
             Statement statement = connection.createStatement();
             ResultSet resultSet = statement.executeQuery("SELECT course_name, " +
                     "COUNT(subscription_date)/" +
                     "(MAX(MONTH(subscription_date)) - MIN(MONTH(subscription_date)) + 1) as avgMonthSales " +
                     "FROM purchaselist " +
                     "WHERE YEAR(subscription_date) = 2018 " +
-                    "GROUP BY course_name;");
+                    "GROUP BY course_name;")) {
             while (resultSet.next()) {
-                String courseName = resultSet.getString("course_name");
-                String avgMonthSales = resultSet.getString("avgMonthSales");
-                System.out.println(courseName + " | Среднее кол-во покупок в месяц: " + avgMonthSales);
+                    String courseName = resultSet.getString("course_name");
+                    String avgMonthSales = resultSet.getString("avgMonthSales");
+                    System.out.println(courseName + " | Среднее кол-во покупок в месяц: " + avgMonthSales);
             }
-            resultSet.close();
-            statement.close();
-            connection.close();
-        }
-        catch(Exception ex){
-            ex.printStackTrace();
         }
     }
 }
