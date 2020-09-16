7b7b9ef0ac8796553af1d942b1893c7c8feb5ab9
EDpanichkin <edpanichkin@gmail.com>
2020-09-16 11:19:09 +0300

    Task 11.4

---
+++ 11_Multithreading/ImageResizer/src/main/java/ImageResizer.java
@@ -0,0 +1,46 @@
+import org.imgscalr.Scalr;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.util.List;
+
+public class ImageResizer implements Runnable {
+    private File[] files;
+    private List<File> lFiles;
+    private int newWidth;
+    private String dstFolder;
+    private long start;
+
+    public ImageResizer(File[] files, int newWidth, String dstFolder, long start) {
+        this.files = files;
+        this.newWidth = newWidth;
+        this.dstFolder = dstFolder;
+        this.start = start;
+    }
+
+    @Override
+    public void run(){
+        try
+        {
+            for(File file : files)
+            {
+                BufferedImage image = ImageIO.read(file);
+                if(image == null) {
+                    continue;
+                }
+                int newHeight = (int) Math.round(
+                        image.getHeight() / (image.getWidth() / (double) newWidth)
+                );
+                BufferedImage newImage = Scalr.resize(image, Scalr.Method.BALANCED, newWidth, newHeight);
+
+                File newFile = new File(dstFolder + "/" + file.getName());
+                ImageIO.write(newImage, "jpg", newFile);
+            }
+        }
+        catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        System.out.println("Finished: " + (System.currentTimeMillis() - start));
+    }
+}
