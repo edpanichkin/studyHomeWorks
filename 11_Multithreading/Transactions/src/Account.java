bea2d33c5dbcaad85ffd508f87f87241806e3905
EDpanichkin <edpanichkin@gmail.com>
2020-09-25 14:55:03 +0300

    Task 11.2

---
+++ 11_Multithreading/Transactions/src/Account.java
@@ -0,0 +1,37 @@
+public class Account
+{
+    public Account(String accNumber, long money) {
+        this.money = money;
+        this.accNumber = accNumber;
+    }
+
+    private long money;
+    private String accNumber;
+    private boolean isFraud = false;
+
+    public boolean isFraud() {
+        return isFraud;
+    }
+
+    public void setFraud(boolean fraud) {
+        isFraud = fraud;
+    }
+
+    public synchronized long getMoney() {
+        return money;
+    }
+
+    public synchronized void setMoney(long money) {
+        this.money = money;
+    }
+
+    public String getAccNumber() {
+        return accNumber;
+    }
+
+    public void setAccNumber(String accNumber) {
+        this.accNumber = accNumber;
+    }
+
+
+}
---
+++ 11_Multithreading/Transactions/src/Bank.java
@@ -0,0 +1,92 @@
+import java.util.HashMap;
+import java.util.Random;
+
+public class Bank
+{
+    private static final String ACC_NUM_FORMAT = "%03d";
+    private static final int TIME_TO_FRAUD_CHECK = 10;
+    private static final int SUM_TO_FRAUD_CHECK = 50000;
+    private int transferCount = 0;
+    private int transferOKCount = 0;
+    private HashMap<String, Account> accounts;
+    private final Random random = new Random();
+
+    public Bank (){
+        this.accounts = new HashMap<>();
+    }
+
+    public synchronized boolean isFraud(String fromAccountNum, String toAccountNum, long amount)
+        throws InterruptedException
+    {
+        Thread.sleep(TIME_TO_FRAUD_CHECK);
+        return random.nextBoolean();
+    }
+
+    public void bankBalance() {
+        System.out.println(accounts.values().stream().mapToLong(Account::getMoney).sum());
+    }
+
+    public void createAccount (long money) {
+        String accNumber = String.format(ACC_NUM_FORMAT, accounts.size() + 1);
+        accounts.put(accNumber, new Account(accNumber, money));
+
+    }
+
+    /**
+     * TODO: реализовать метод. Метод переводит деньги между счетами.
+     * Если сумма транзакции > 50000, то после совершения транзакции,
+     * она отправляется на проверку Службе Безопасности – вызывается
+     * метод isFraud. Если возвращается true, то делается блокировка
+     * счетов (как – на ваше усмотрение)
+     */
+
+    public void transfer(String fromAccountNum, String toAccountNum, long amount) throws InterruptedException {
+        transferCount++;
+        if(!accountFraudCheck(fromAccountNum, toAccountNum)
+                && accounts.get(fromAccountNum).getMoney() >= amount)
+        {
+            if ((amount > SUM_TO_FRAUD_CHECK)) {
+                if (isFraud(fromAccountNum, toAccountNum, amount)) {
+                    blockAfterFraudCheck(fromAccountNum, toAccountNum);
+                } else {
+                    transferOperationApproved(fromAccountNum, toAccountNum, amount);
+                }
+            }
+            transferOperationApproved(fromAccountNum, toAccountNum, amount);
+        }
+    }
+
+    private boolean accountFraudCheck(String fromAccountNum, String toAccountNum) {
+        return  accounts.get(fromAccountNum).isFraud() && accounts.get(toAccountNum).isFraud();
+    }
+    private void blockAfterFraudCheck(String fromAccountNum, String toAccountNum){
+        accounts.get(fromAccountNum).setFraud(true);
+        accounts.get(toAccountNum).setFraud(true);
+    }
+
+    /**
+     * TODO: реализовать метод. Возвращает остаток на счёте.
+     */
+    public long getAccountBalance(String accountNum)
+    {
+        return accounts.get(accountNum).getMoney();
+    }
+
+    public void printBlockedCount() {
+        System.out.println("Blocked accounts: " + accounts.values().stream().filter(Account::isFraud).count()
+                + " | Active accounts: " + accounts.values().stream().filter(a -> !a.isFraud()).count());
+
+    }
+    private void transferOperationApproved (String fromAccountNum, String toAccountNum, long amount){
+        transferOKCount++;
+        accounts.get(fromAccountNum).setMoney(accounts.get(fromAccountNum).getMoney() - amount);
+        accounts.get(toAccountNum).setMoney(accounts.get(toAccountNum).getMoney() + amount);
+    }
+    public void debugPrint(){
+        bankBalance();
+        System.out.printf("Transfers %s, OK %s\n",
+            transferCount, transferOKCount);
+}
+
+
+}
