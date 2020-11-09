21dadf385e7e9a65f0fcaf45cd1b7262d15d7c08
Egor Panichkin <edpanichkin@gmail.com>
2020-11-09 12:24:31 +0300

    Task 12.1 http://localhost:8080/date or http://localhost:8080/random

---
+++ 12_SpringBootWebDevelopment/ToDoList/src/main/java/main/DefaultController.java
@@ -0,0 +1,19 @@
+package main;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+
+@RestController
+public class DefaultController
+{
+    @RequestMapping("/date")
+    public String date(){
+        return (new Date()).toString();
+    }
+    @RequestMapping("/random")
+    public String random(){
+        return String.valueOf(Math.random());
+    }
+}
