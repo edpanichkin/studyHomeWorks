2c3272258b5636712174f73958ecc5c82b111fde
Egor  Panichkin <edpanichkin@gmail.com>
2022-03-01 13:22:40 +0300

    Changed "fill LinkedPurchaseList"

--- 10_SQLAndHibernate/JDBCskillboxSQLHibernate/src/main/java/Main.java
+++ 10_SQLAndHibernate/JDBCskillboxSQLHibernate/src/main/java/Main.java
@@ -1,45 +1,61 @@
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
 import org.hibernate.boot.Metadata;
 import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
-import org.hibernate.query.Query;
 import skillboxsql.*;
 
-import java.util.Date;
-import java.util.List;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args){
-        StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
-                .configure("hibernate.cfg.xml").build();
-        Metadata metadata = new MetadataSources(registry).getMetadataBuilder().build();
-        try(SessionFactory sessionFactory = metadata.getSessionFactoryBuilder().build();
-            Session session = sessionFactory.openSession())
-        {
-            fillLinkedPurchaseList(session);
-        }
-    }
+  public static void main(String[] args) {
 
-    public static void fillLinkedPurchaseList(Session session){
-        List<Object[]> pList = session.createNativeQuery(
-                "SELECT student_name, course_name FROM purchaselist").getResultList();
-        for (Object[] pow: pList){
-            session.beginTransaction();
-            int sId = Integer.parseInt(session.createQuery("SELECT S.id FROM Student S WHERE name = :sname").
-                    setParameter("sname", pow[0])
-                    .getResultList().toString()
-                    .replace("[","").replace("]",""));
-
-            int cId = Integer.parseInt(session.createQuery("SELECT C.id FROM Course C WHERE name = :cname")
-                    .setParameter("cname", pow[1])
-                    .getResultList().toString()
-                    .replace("[","").replace("]",""));
-            LinkedPurchaseList linkedPL = new LinkedPurchaseList(sId, cId);
-            session.save(linkedPL);
-            session.getTransaction().commit();
-        }
+    StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+            .configure("hibernate.cfg.xml").build();
+    Metadata metadata = new MetadataSources(registry).getMetadataBuilder().build();
+    try (SessionFactory sessionFactory = metadata.getSessionFactoryBuilder().build();
+         Session session = sessionFactory.openSession()) {
+      //List listTables = session.createSQLQuery("show tables from skillbox").list();
+      fillLinkedPurchaseList(session);
     }
+  }
+
+  public static void fillLinkedPurchaseList(Session session) {
+    session.beginTransaction();
+
+    List<Object[]> pList = session.createNativeQuery(
+            "SELECT student_name, course_name FROM purchaselist").getResultList();
+    List<Object[]> sList = session.createNativeQuery(
+            "SELECT id, name FROM students").getResultList();
+    List<Object[]> cList = session.createNativeQuery(
+            "SELECT id, name FROM courses").getResultList();
+
+    Map<Integer, String> studentsMap = new HashMap<>();
+    Map<Integer, String> coursesMap = new HashMap<>();
+
+    for (Object[] student : sList) {
+      studentsMap.put((Integer) student[0], (String) student[1]);
+    }
+    for (Object[] course : cList) {
+      coursesMap.put((Integer) course[0], (String) course[1]);
+    }
+    for (Object[] pow : pList) {
+      int studentIndex = indexReturn(studentsMap, (String) pow[0]);
+      int courseIndex = indexReturn(coursesMap, (String) pow[1]);
+      LinkedPurchaseList linkedPL = new LinkedPurchaseList(studentIndex, courseIndex);
+      session.saveOrUpdate(linkedPL);
+    }
+    session.getTransaction().commit();
+  }
+
+  public static int indexReturn(Map<Integer, String> map, String searchIndex) {
+    Set<Map.Entry<Integer, String>> entrySet = map.entrySet();
+    for (Map.Entry<Integer, String> pair : entrySet) {
+      if (searchIndex.equals(pair.getValue())) {
+        return pair.getKey();
+      }
+    }
+    return 0;
+  }
 }
